c981697bb83aff3c8c33ea3b768297b74d88e062
Javon Harper <javharper@gmail.com>
2011-12-22 22:42:13 -0500

    some readme and library file check stuff

---
+++ README.txt
@@ -0,0 +1,7 @@
+The Beethoven Album Art Fetcher
+
+Beethoven is a python application that fetches album art from the Internet and saves the images in the metadata of your music files.
+The name is a play on the famous classical composer Ludwig van Beethoven and Beethoven the beloved mischievous dog.
+
+How Do I Use it:
+I dunno, lol.
--- beethoven/library.py
+++ beethoven/library.py
@@ -1,13 +1,27 @@
-class LibraryModel:
-  def get_music_collection(self, path):
-    return 0
-  # given a path
+import os.path
+
   # find every file in it that is a (wma, mp3, wav) recursively
   # get the artist
   # get the album
   # get the path
   # add to a map of the form Map[<artist name>, Map[<album name>, <song path>]]
   # return this map
+
+class LibraryModel:
+  def get_music_collection(self, path):
+    if self.music_directory_is_valid(path):
+      return self.build_music_map(path)
+    print path, 'is not a valid directory.'
+    return None
+
+  def music_directory_is_valid(self, path):
+    return os.path.exists(path) and os.path.isdir(path)
+  
+  def build_music_map(self, path):
+    return None
+#    for root, dirs, files in os.walk(path):
+#      for filename in files:
+#        print filename.endswith('.mp3')
 
 library_model = LibraryModel()
 library_data = library_model.get_music_collection('/home/javon/programming/projects/beethoven/music-dir')
